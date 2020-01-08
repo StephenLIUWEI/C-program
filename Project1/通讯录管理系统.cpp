f5fdd40e00797352285e27f20edf9f8478c7109e
wei.liu <wei.liu@lifesense.com>
2020-01-08 09:43:56 +0800

    1.实现通讯录添加功能

--- Project1/通讯录管理系统.cpp
+++ Project1/通讯录管理系统.cpp
@@ -1,7 +1,94 @@
 #include<iostream>
 using namespace std;
 
+#define MAX_NUMBER  100
 
+//设计联系人结构体
+struct Person
+{
+	string m_Name;
+
+	int m_Sex;
+
+	int m_Age;
+
+	string m_Phone;
+
+	string m_Addr;
+};
+
+//通讯录结构体 
+struct Addressbooks
+{
+	struct Person personArr[MAX_NUMBER];
+
+	int m_Size;
+};
+
+//添加联系人
+void addPerson(Addressbooks* abs)
+{
+	if (abs->m_Size == MAX_NUMBER)
+	{
+		cout << "通讯录已满！" << endl;
+		return ;
+	}
+	else
+	{
+		//添加姓名
+		string name;
+		cout << "请输入姓名: " << endl;
+		cin >> name;
+		abs->personArr[abs->m_Size].m_Name = name;
+
+		//添加性别
+		int sex;
+		cout << "请输入性别: " << endl;
+		cout << "1----男: " << endl;
+		cout << "2----女: " << endl;
+
+		while (true)
+		{
+			cin >> sex;
+			if (sex == 1 || sex == 2)
+			{
+				abs->personArr[abs->m_Size].m_Sex = sex;
+				break;
+			}
+			cout << "输入有误，请重新输入 " << endl;
+		}
+
+		//添加年龄
+		int age;
+		cout << "请输入年龄: " << endl;
+		cin >> age;
+		abs->personArr[abs->m_Size].m_Age = age;
+
+		//添加电话
+		string phone;
+		cout << "请输入电话: " << endl;
+		cin >> phone;
+		abs->personArr[abs->m_Size].m_Phone = phone;
+
+		//添加住址
+		string addr;
+		cout << "请输入地址: " << endl;
+		cin >> addr;
+		abs->personArr[abs->m_Size].m_Addr = addr;
+
+		//更新通讯录
+		abs->m_Size++;
+
+		cout << "添加成功！ " << endl;
+
+		system("pause"); //任意键退出
+		system("cls"); //清屏
+	}
+
+
+
+
+}
 
 //menu
 void showmenu()
@@ -20,6 +107,10 @@ void showmenu()
 
 int main()
 {
+	//创建通讯录结构体变量
+	Addressbooks abs;
+	abs.m_Size = 0;
+
 	int select = 0; //创建用户输入变量
 
 	while (true)
@@ -30,7 +121,8 @@ int main()
 
 		switch (select)
 		{
-		case 1:
+		case 1: //添加联系人
+			addPerson(&abs);
 			break;
 		case 2:
 			break;
